ac087cfaee52691145a1ff0a7b5065e95c17497c
Aris Kuckovic <ak@abtion.com>
2023-01-03 16:07:34 +0100

    Prettified comments.

--- functions/health.php
+++ functions/health.php
@@ -7,12 +7,19 @@ function abtion_health_check() {
 
   $email_body = [];
   
-  // Check if site has any "critical" issues (Site Health)
+  /**
+   * -------------------------------------------------------
+   * Check if site has any "critical" issues (Site Health) *
+   * -------------------------------------------------------
+  */
   if($site_health_obj->critical >= 1) {
     $email_body['Critical'] = 'There are one or more critical errors on this site.';
   }
 
-  // Check when SSL-certificate is set to expire
+  /** --------------------------------------------
+   * Check when SSL-certificate is set to expire *
+   * ---------------------------------------------
+  */
   $original_parse = parse_url(get_site_url(), PHP_URL_HOST);
   $get = stream_context_create(array('ssl' => array('capture_peer_cert' => true)));
   $read = stream_socket_client('ssl://' . $original_parse . ':443', $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $get);
@@ -28,6 +35,7 @@ function abtion_health_check() {
     $email_body['SSL'] = date('d/m/Y H:i:s', $expires);
   }
 
+  // Echo for now - send mail later...
   echo '<pre>' . print_r($email_body, true) . '</pre>';die;
 
 }
