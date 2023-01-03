6de6e09b7500a485a1dcf6f2fa1cd1a61fbda6af
Aris Kuckovic <ak@abtion.com>
2023-01-03 11:07:42 +0100

    Version 0.1 - Unused scripts & styles

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# abtion-optimize-plugin
+# Abtion Optimize - WP Plugin
+
+This plugin removes unused scripts and styles from existing WordPress sites. It also monitors
+the current "Site Health" score, and reports to service@abtion.com if any attention is needed.
+The plugin will not interfere with any content or theme-files, and therefore it will **not** cause any
+errors on an existing WordPress site.
+
+# Supported WordPress versions
+
+This plugin will work on WordPress 5.2 and onward  
+It will not cause any issues on prior versions, but "Site Health" was not introduced until 5.2
+
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+/**
+* Plugin Name: Abtion Optimize
+* Plugin URI: http://abtion.com
+* Description: This site is optimized by Abtion
+* Author: Abtion.com
+* Author URI: http://abtion.com/
+* Version: 0.1
+* License: GPLv2 or later
+*/
+
+/** No direct access */
+if(!defined('ABSPATH')) {
+  exit;
+}
+
+/** Current plugin path */
+define('PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+/** Add the optimizing functions */
+$autoload = ['functions/*.php'];
+foreach ($autoload as $path) {
+  $relative = PLUGIN_DIR .$path;
+  $files = (is_file($relative)) ? [$relative] : glob($relative);
+  foreach($files as $file) { 
+    require_once $file; 
+  }
+}
+
+/** 
+ * ----------------------------------
+ * Simple 'remove_action' functions *
+ * ----------------------------------
+ */
+
+// Remove WordPress version number
+remove_action('wp_head', 'wp_generator');
+
+// Remove RSS feed links + Extra feed links
+remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'feed_links_extra', 3);
+
+// Remove link for "Windows Live Writer
+remove_action('wp_head', 'wlwmanifest_link');
+
+// Remove Adjacent Post Links
+remove_action('wp_head', 'adjacent_posts_rel_link');
+
+// Remove shortlink tag (no need for 2 links (Bad SEO))
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
